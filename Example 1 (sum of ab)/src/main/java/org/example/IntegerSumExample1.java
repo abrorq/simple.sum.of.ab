bb5146da89aac0c77c27df9b22b8fb2a336c2a8e
Abrorbek <134126677+abrorq@users.noreply.github.com>
2023-08-11 22:32:10 +0500

    Add files via upload

---
+++ Example 1 (sum of ab)/src/main/java/org/example/IntegerSumExample1.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class IntegerSumExample1 {
+
+    public static void main(String[] args) {
+        int a = 156489;
+        int b = 398489455;
+        // It will return the sum of a and b.
+        System.out.println("The sum of a and b is = " + Integer.sum(a, b));
+    }
+}
